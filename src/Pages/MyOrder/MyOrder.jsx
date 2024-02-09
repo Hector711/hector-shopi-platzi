51044650e0afd1efa1ac15de0f07ca3900178b51
Héctor Guerra <hectorluengo.xx@gmail.com>
2024-02-09 22:14:21 +0100

    Rutas configuradas

---
+++ src/Pages/MyOrder/MyOrder.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function MyOrder() {
+
+  return (
+    <div>
+      MyOrder
+    </div>
+  )
+
+}
+
+export default MyOrder
---
+++ src/Pages/MyOrders/MyOrders.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function MyOrders() {
+
+  return (
+    <div>
+      MyOrder
+    </div>
+  )
+
+}
+
+export default MyOrders
