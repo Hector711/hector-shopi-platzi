68b9d2aa2b9c45cef98230c5f79f7965e51ac240
Héctor Guerra <hectorluengo.xx@gmail.com>
2024-02-12 01:53:06 +0100

    adicion de productos

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,13 +1,17 @@
-function Card() {
+import React from "react";
+
+function Card({ data }) {
   return (
-    <div className="bg-white cursor-pointer w-87 h-60 rounded-lg">
+    <div className="bg-white cursor-pointer w-50 h-52 rounded-lg">
       <figure className="relative mb-4 w-full h-4/5">
+      {data && data.category && ( // Verificar si data y data.category están definidos
         <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 p-1">
-          Electronics
+            {data.category.name}
         </span>
+      )}
         <img
           className="w-full h-full object-cover rounded-lg"
-          src="https://images.pexels.com/photos/1037992/pexels-photo-1037992.jpeg"
+          src={data ? data.images : ""}
           alt="Headphones"
         />
         <div className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2">
@@ -16,10 +20,13 @@ function Card() {
       </figure>
 
       <p className="flex justify-between">
-        <span className="text-sm font-light m-1">Headphones</span>
+        <span className="text-sm font-light m-1">{data ? data.title : ""}</span>
         <span className="text-lg font-medium">$300</span>
       </p>
     </div>
   );
 }
 export default Card;
+
+
+
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
-import Layout from '../../Components/Layout/Layout'
-import Card from '../../Components/Card/Card'
-
+import React, { useState, useEffect } from "react";
+import Layout from "../../Components/Layout/Layout";
+import Card from "../../Components/Card/Card";
 
 function Home() {
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products")
+      .then((response) => response.json())
+      .then((data) => setItems(data))
+      // .then(data => console.log(data))
+      .catch((error) => console.error("Error fetching data:", error)); // Manejo de errores
+  }, []);
 
   return (
     <Layout>
-      Home 
-
-    <Card />
+      <h1>Home</h1>
+      <section className="flex justify-center flex-wrap gap-5 p-20">
+        {items?.map((item) => (
+          <Card key={item.id} data={item} />
+        ))}
+        <Card />
+      </section>
     </Layout>
-  )
+  );
 }
 
-export default Home
+export default Home;
